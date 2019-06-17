18098ed89564e01d2f8b88f54de58add692c7bb5
Claudio Altamiranda <claudioaltamiranda@yahoo.com.ar>
2019-06-17 09:06:53 -0300

    Google SignIn

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express')
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express()
 const bodyParser = require('body-parser');
@@ -9,9 +10,12 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, './public')));
+// console.log(path.resolve(__dirname, './public'));
+
 // Configuración global de rutas
 app.use(require('./routes/index'));
-
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
     (err, res) => {
